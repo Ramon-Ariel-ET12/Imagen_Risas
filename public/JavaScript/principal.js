bd80003122c45b74cf7816ede8daca610f6ffae2
thomas <mamanithomasfabricio@gmail.com>
2023-10-23 16:56:50 -0300

    Updated

--- public/JavaScript/principal.js
+++ public/JavaScript/principal.js
@@ -12,3 +12,20 @@ profileImage.addEventListener("click", function() {
         document.body.removeChild(fullscreenImage);
     });
 });
+function toggleMenu() {
+    var menu = document.getElementById("menuOptions");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+function previewImage(event) {
+    var image = document.getElementById("profile-image");
+    var file = event.target.files[0];
+    
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            image.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}
